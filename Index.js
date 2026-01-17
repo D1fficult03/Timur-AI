169f7670b13eb6dda6e2c0151ef9c970053da545
D1fficult03 <yandere2024@outlook.com>
2026-01-18 00:10:35 +0100

    Index.js

--- Index.js
+++ Index.js
@@ -15,7 +15,7 @@ export default async ({ req, res }) => {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
-          "Authorization": `Bearer ${process.env.sk-08dbe8cc0ab3431787da1c4f9e9f53ae}`
+          "Authorization": `Bearer ${process.env.DEEPSEEK_API_KEY}`
         },
         body: JSON.stringify({
           model: "deepseek-chat",
